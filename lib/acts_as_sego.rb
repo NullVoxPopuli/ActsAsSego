c649e64a1b59038767666af5761e6d77d309d862
NullVoxPopuli <LPSego3+dev@gmail.com>
2014-10-11 14:34:50 -0400

    search through all methods, not just instance methods

--- lib/acts_as_sego.rb
+++ lib/acts_as_sego.rb
@@ -3,24 +3,28 @@ module ActsAsSego
   module Intercepter
 
     def self.included(base)
-      # base.send(:capture, self, :define_method) do |method, result, args|
-      #   puts "Yay Dynamically defined methods!"
-      # end
+      base.send(:capture, base.class, :define_method) do |method, result, args|
+        puts "Yay Dynamically defined methods!"
+      end
     end
 
     # simplied from:
     # http://code.google.com/p/rcapture/
+    #
+    # TODO: figure out how to make this work with
+    # methods defined after the inclusion of this module
     def capture(klass, method, &callback)
-      previous_method = klass.instance_method(method)
+      previous_method = klass.method(method)
 
       klass.class_eval do
         define_method "#{method}" do |*args, &block|
-          result = previous_method.bind(self).call(*args, &block)
+          result = previous_method.call(*args, &block)
           callback.call(method, result, args)
           result
         end
       end
     end
   end
-
 end
+
+Object.send(:include, ActsAsSego::Intercepter)
